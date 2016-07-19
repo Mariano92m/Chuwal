5f6a9843e2055bf7fb12399eed060efe67aebbff
Mariano92m <zeta007m@gmail.com>
2016-07-18 21:23:23 -0300

    asd

---
+++ controllers/chuInterface.py
@@ -0,0 +1,29 @@
+def chuHome():
+	if session.nickname!='':
+        return dict()
+    else:
+        redirect(URL('chuInterface','chuRegister'))
+
+def chuProfile():
+	if session.nickname!='':
+        return dict()
+    else:
+        redirect(URL('chuInterface','chuRegister'))
+
+def chuUs():
+	return dict()
+
+def chu404():
+	return dict()
+
+def chuHelp():
+	return dict()
+
+def chuWelcome():
+	return dict()
+
+def chuRelated():
+	return dict()
+
+def chuRegister():
+	return dict()
